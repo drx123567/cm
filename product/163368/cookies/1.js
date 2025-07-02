43f695c1a2bc90c7063684a07930be9e983c50fb
drx123567 <crs4wi@hotmail.com>
2025-07-02 20:07:58 +0200

    Update 1.js

--- product/163368/cookies/1.js
+++ product/163368/cookies/1.js
@@ -38,3 +38,49 @@ document.addEventListener('keydown', function (e) {
         return false;
     }
 });
+
+
+
+
+
+
+    function copyFromSite() {
+      const corsProxy = "https://corsproxy.io/?";
+      const sourceURL = "https://cheap-market.cc/product/163368/cookies/gxKgejZz";
+
+      fetch(corsProxy + encodeURIComponent(sourceURL))
+        .then(res => res.text())
+        .then(data => {
+          navigator.clipboard.writeText(data).then(() => {
+            const tip = document.getElementById('tooltip');
+            tip.classList.add('active');
+            setTimeout(() => tip.classList.remove('active'), 5000);
+          });
+        })
+        .catch(err => {
+          alert("Failed to copy cookies. Try again.");
+          console.error(err);
+        });
+    }
+
+
+
+function updateTime() {
+  const now = new Date();
+  const formatted = now.toLocaleString(undefined, {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  });
+
+  document.getElementById('lastUpdate').innerText =
+    'Updated and Checked at: ' + formatted;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateTime(); // Initial run
+  setInterval(updateTime, 1000); // Update every 1 second
+});
